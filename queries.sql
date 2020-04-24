bcef02e1c9c5388f5597f97f38b5e80ec727dbf5
Jacob Wade <jacoblorenwade@Jacobs-MacBook-Pro.local>
2020-04-23 18:46:25 -0700

    Added module queries

--- queries.sql
+++ queries.sql
@@ -169,9 +169,75 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
+-- NEW retirement employment table
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1951-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
 
+-- Manager retirement table
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.first_name,
+	ce.last_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments as d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce
+		ON (dm.emp_no = ce.emp_no);
+	
+-- Department Retirees table
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp as de
+		ON (de.emp_no = ce.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
 
+-- Retirement info in the Sales department
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp as de
+		ON (de.emp_no = ce.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
 
+-- Retirement info in the Sales and Development departments
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_development_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp as de
+		ON (de.emp_no = ce.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
 
 
 
